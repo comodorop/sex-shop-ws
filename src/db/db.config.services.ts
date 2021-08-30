52bbbff7ef6757b63fd1c6e3724bd47cbcf0787b
Pablo Torres <pablotorres@Pablos-MacBook-Air.local>
2021-08-30 16:01:01 -0500

    add config postgres

--- src/db/db.config.services.ts
+++ src/db/db.config.services.ts
@@ -1,4 +1,21 @@
 
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 export const dataBaseProvider = [
-    
+    TypeOrmModule.forRootAsync({
+        imports: [],
+        inject: [],
+        async useFactory() {
+            return {
+                type: 'postgres',
+                host: 'localhost',
+                port: 5432,
+                username: 'postgres',
+                password: '123456',
+                database: 'ecomerce',
+                __dirname: '/../**/infraestructure/schema/*.schema{.ts,.js}',
+                synchronize: true,
+            }
+        },
+    })
 ]
--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -3,10 +3,11 @@ https://docs.nestjs.com/modules
 */
 
 import { Module } from '@nestjs/common';
-
+import { dataBaseProvider } from './db.config.services'
 @Module({
-    imports: [],
+    imports: dataBaseProvider,
     controllers: [],
     providers: [],
+    exports: dataBaseProvider
 })
-export class DbModule {}
+export class DbModule { }
