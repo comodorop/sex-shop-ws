817fd7ef8f883d4347c331019fff6163375fbf86
comodorop <comodoro_21@hotmail.com>
2021-09-02 10:46:59 -0500

    add new interfaces

--- src/products/application/services/products.service.ts
+++ src/products/application/services/products.service.ts
@@ -8,8 +8,6 @@ import { BaseCreateProduct } from '../interfaces/base.createProduct.interface';
 
 @Injectable()
 export class ProductsService implements BaseCreateProduct {
-
-
     async createProduct(createDto: CreateProductDTO): Promise<CreateProductDTO> {
         return createDto
     }
